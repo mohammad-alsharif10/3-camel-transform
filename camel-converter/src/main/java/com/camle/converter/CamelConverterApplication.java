54caf7e22a6b087516bab25c91334460f1d69abe
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2022-02-08 23:21:30 +0200

    Initial commit

---
+++ camel-converter/src/main/java/com/camle/converter/CamelConverterApplication.java
@@ -0,0 +1,13 @@
+package com.camle.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelConverterApplication.class, args);
+    }
+
+}
