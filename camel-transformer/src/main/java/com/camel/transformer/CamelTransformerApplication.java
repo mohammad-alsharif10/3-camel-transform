4e1b760aef49056c0e6b7683876b1b188be5112a
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2022-02-08 23:37:07 +0200

    init

---
+++ camel-transformer/src/main/java/com/camel/transformer/CamelTransformerApplication.java
@@ -0,0 +1,13 @@
+package com.camel.transformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelTransformerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelTransformerApplication.class, args);
+    }
+
+}
---
+++ camel-transformer/src/test/java/com/camel/transformer/CamelTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.transformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelTransformerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
